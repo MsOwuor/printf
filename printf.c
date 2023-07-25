52d8df767a782492a2c59cb209cad1887571d5d6
vic-kip <vcherutich19@gmail.com>
2023-07-25 05:35:07 -0700

    update

--- printf.c
+++ printf.c
@@ -2,85 +2,59 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdarg.h>
-#include <stdlib.h>
 
 /**
- * get_func -  select the function to handle the format
- * @format: format
- * Return: function pointer to the corresponding function
+ * _printf - prints the function printf
+ * @args: args
+ * Return: Prints char
  */
 
-int (*get_func(char format))(va_list)
-{
-	switch (format)
-	{
-		case 'c':
+int print_char(va_list args);
+int print_string(va_list args);
+int print_decimal(va_list args);
+int print_int(va_list args);
 
-			return (&print_char);
-			
-		case 's':
-			return (&print_string);
-
-		case 'd':
-			return (&print_decimal);
-		case 'i':
-			return (&print_int);
-		default:
-			return (NULL);
-	}
-}
-
-
-/**
- * _printf -  function that prints output
- * @format: character string, composed of zero and more directives
- *
- * Return:  the number of characters print
- */
+int (*get_func(char format))(va_list);
 
 int _printf(const char *format, ...)
 {
 	if (format == NULL)
-	{
-		int char_printed = 0;
-		int x;
-		int (*i)(va_list);
-		va_list list;
-
-		va_start(list, format);
-		x = 0;
-
-		if (format[0] == '%' && format[1] == '\0')
-			return (-1);
-
-		while (format == NULL && format[x] == '\0')
-		{
-			if (format[x] == '%')
-			{
-				if (format[x + 1] == '%')
-				{
-					char_printed += _putchar(format[x]);
-					x += 2;
-				}
-				else
-				{
-					i = get_func(format[x + 1]);
-					if (i)
-						char_printed += i(list);
-					else
-						char_printed = _putchar(format[x]) + _putchar(format[x + 1]);
-					x += 2;
-				}
-			}
-			else
-			{
-				char_printed += _putchar(format[x]);
-				x++;
-			}
-		}
-		va_end(list);
-
-		return (char_printed);
-	}
 	return (-1);
+
+	int char_printed = 0;
+	int x;
+	int (*i)(va_list);
+	va_list list;
+
+	va_start(list, format);
+	x = 0;
+
+	while (format[x] != '\0')
+	{
+	if (format[x] == '%')
+	{
+	if (format[x + 1] == '%')
+	{
+	char_printed += _putchar(format[x]);
+	x += 2;
+	}
+	else
+	{
+	i = get_func(format[x]);
+	if (i)
+	char_printed += i(list);
+	else
+	char_printed += _putchar(format[x]) + _putchar(format[x + 1]);
+	x += 2;
+	}
+	}
+	else
+	{
+	char_printed += _putchar(format[x]);
+	x++;
+	}
+	}
+	va_end(list);
+
+	return (char_printed);
 }
