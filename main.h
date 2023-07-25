2e27c8efff52b88cd3717d6db6ebfb1d1bcebd5f
vic-kip <vcherutich19@gmail.com>
2023-07-25 11:55:38 -0700

    update

--- main.h
+++ main.h
@@ -24,4 +24,7 @@ int print_char(va_list list);
 int print_string(va_list list);
 int print_decimal(va_list list);
 int print_int(va_list list);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
+
 #endif /* MAIN_H */
